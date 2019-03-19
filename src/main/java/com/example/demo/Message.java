e5c23f78ff2f6a4e6aee400b506a5c4afeed4411
Yohannesns <37091801+Yohannesns@users.noreply.github.com>
2019-03-19 18:17:25 -0400

    first commit

---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,62 @@
+package com.example.demo;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min = 10)
+    private String content;
+
+    @NotNull
+    @DateTimeFormat(iso= DateTimeFormat.ISO.DATE)
+    private Date postedDate;
+
+    @NotNull
+    private String sentBy;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getPostedDate() {
+        return postedDate;
+    }
+
+    public void setPostedDate(Date postedDate) {
+        this.postedDate = postedDate;
+    }
+
+    public String getSentBy() {
+        return sentBy;
+    }
+
+    public void setSentBy(String sentBy) {
+        this.sentBy = sentBy;
+    }
+}
